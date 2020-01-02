93dd14071ddc095e6690b5f9c95c5324dcab0561
Leopold0801 <982539404@qq.com>
2020-01-02 15:10:25 +0800

    remove do not needed

--- a.py
+++ a.py
@@ -7,11 +7,9 @@ ffffffff
 jjjjjjjjj
 uuuuuuuu edit by hot_fix
 uuuuuuuu   edit by master
-<<<<<<< HEAD
 test git_gui
 
 edit by apple
 
-=======
 edit by zjh
->>>>>>> 5ceae8c73d63bc7b148e66cfe49448fa0b91bf52
+
